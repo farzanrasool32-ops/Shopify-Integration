12e5d195c41759fb70001ffe06f23d63ebad3182
farzanrasool32-ops <farzanrasool32@gmail.com>
2026-07-22 23:44:22 +0500

    Implement order synchronization functionality and add sync button; update order key to orderId

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -15,16 +15,40 @@ const Orders = () => {
       setLoading(false);
     }
   };
-
   useEffect(() => {
     getOrders();
   }, []);
+
+  const syncOrders = async () => {
+    try {
+      setLoading(true);
+
+      await API.post("integration/orders/sync");
+
+      await getOrders();
+
+      alert("Orders synced successfully");
+    } catch (error) {
+      alert(error.response?.data?.message || "Failed to sync orders");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="p-8">
       <div className="bg-[#1E293B] rounded-xl p-6 shadow-lg">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-3xl font-bold text-white">Shopify Orders</h1>
+
+          <div className="flex gap-3 ites-center">
+            <button
+              onClick={syncOrders}
+              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
+            >
+              Sync Orders
+            </button>
+          </div>
 
           <span className="bg-[#95BF47] text-black px-4 py-2 rounded-lg font-semibold">
             Total Orders : {orders.length}
@@ -52,7 +76,7 @@ const Orders = () => {
               <tbody>
                 {orders.map((order) => (
                   <tr
-                    key={order.id}
+                    key={order.orderId}
                     className="border-b border-gray-700 hover:bg-[#111827] transition"
                   >
                     <td className="p-4 text-white">{order.name}</td>
